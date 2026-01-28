e4d45667e09ecec172c45481c865663ae24f96f1
Sk Md Jeesan <skmdjeesan@gmail.com>
2026-01-28 23:42:14 +0530

    sponsors done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,21 @@
 import EventShowcase from "@/components/EventShowcase";
 import FaqSection from "@/components/FAQ";
 import Footer from "@/components/Footer";
+import Gallery from "@/components/Gallery";
 import HeroSection from "@/components/HeroSection";
 import HomeAbout from "@/components/HomeAbout";
+import Sponsors from "@/components/Sponsors";
 //import { userData } from "@/context/UserContext";
 
 
 export default function Home() {
   return (
     <main className="bg-black w-full h-screen">
-      <HeroSection/>
+      {/* <HeroSection/> */}
       {/* <HomeAbout/>
       <EventShowcase/> */}
       <FaqSection/>
+      <Sponsors />
       <Footer />
     </main>
   );
