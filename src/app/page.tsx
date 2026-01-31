9b189d47f0360e962e0d7eeb8013a4a8c2571474
Sk Md Jeesan <skmdjeesan@gmail.com>
2026-01-31 12:20:30 +0530

    some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
       <HeroSection/>
       {/* <HomeAbout/>
       <EventShowcase/> */}
+      <EventShowcase/>
       <FaqSection/>
       <Footer />
     </main>
