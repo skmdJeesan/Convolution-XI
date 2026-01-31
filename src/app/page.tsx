5fd5fc5c424779f4452a87a4543cea2112b416bc
Sohel Mondal <mondalsohel47025@gmail.com>
2026-01-31 15:11:29 +0530

    feat: updated hero page layout and animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,37 @@
+// src/app/page.tsx
+'use client';
+
+import { useState, useEffect } from 'react';
 import EventShowcase from "@/components/EventShowcase";
 import FaqSection from "@/components/FAQ";
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import HomeAbout from "@/components/HomeAbout";
-import Image from "next/image";
 
 export default function Home() {
+  const [isMobile, setIsMobile] = useState(false);
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    // Signal that the component has safely moved to the client side
+    setHasMounted(true);
+
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <main className="bg-black w-full h-screen">
-      <HeroSection/>
-      <HomeAbout/>
-      <EventShowcase/>
-      <FaqSection/>
+    <main className="bg-black w-full min-h-screen">
+      <HeroSection />
+      <HomeAbout />
+      <EventShowcase />
+      <FaqSection />
       <Footer />
     </main>
-   
   );
 }
