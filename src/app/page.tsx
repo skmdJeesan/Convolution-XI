90ea862cb416c69ceff520f97219fc30d9d89c7b
Sk Md Jeesan <skmdjeesan@gmail.com>
2026-01-31 21:13:28 +0530

    team done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,18 @@ import FaqSection from "@/components/FAQ";
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import HomeAbout from "@/components/HomeAbout";
+import Team from "@/components/Team";
 //import { userData } from "@/context/UserContext";
 
 
 export default function Home() {
   return (
     <main className="bg-black w-full h-screen">
-      <HeroSection/>
+      {/* <HeroSection/> */}
       {/* <HomeAbout/>
       <EventShowcase/> */}
       <FaqSection/>
+      <Team />
       <Footer />
     </main>
   );
