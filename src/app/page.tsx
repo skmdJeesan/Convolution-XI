ad425bdd3c2fee202d90eb8f5002bdb8a564bb7a
Sohom Bhowmick <sohombhowmick14@gmail.com>
2026-02-01 21:05:21 +0530

    added about and gallery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,25 @@
-import EventShowcase from "@/components/EventShowcase";
+import Gallery from "@/components/Gallery"; 
 import FaqSection from "@/components/FAQ";
 import Footer from "@/components/Footer";
-import HeroSection from "@/components/HeroSection";
 import HomeAbout from "@/components/HomeAbout";
-import Image from "next/image";
+import HeroSection from "@/components/HeroSection";
+import Teams from "@/components/Teams";
 
 export default function Home() {
   return (
-    <main className="bg-black w-full h-screen">
-      <HeroSection/>
-      <HomeAbout/>
-      <EventShowcase/>
-      <FaqSection/>
-      <Footer />
+    <main className="bg-black w-full min-h-screen">
+      
+      {/* <HeroSection /> */}
+      <HomeAbout />
+
+      <Gallery />
+
+      {/* <Teams /> */}
+
+      <div className="relative z-10 bg-black">
+        <FaqSection />
+        <Footer />
+      </div>
     </main>
-   
   );
 }
