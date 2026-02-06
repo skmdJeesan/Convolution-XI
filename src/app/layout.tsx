e0bfd682df4d4bb7796d033299387124f9fc1564
Sk Md Jeesan <skmdjeesan@gmail.com>
2026-02-06 16:00:03 +0530

    custom cursor added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import ClientProvider from "@/components/ClientProvider";
+import CustomCursor from "@/components/CustomCursor";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
       > 
         <ClientProvider>
           <Navbar/>
+          <CustomCursor />
           {children}
         </ClientProvider>
       </body>
