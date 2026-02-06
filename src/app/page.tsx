c409edb01b5e34a7f7a15dd3272c49e53632cfa1
SayanIndra83 <sayanindra0@gmail.com>
2026-02-06 19:15:18 +0530

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
       <Gallery/>
       <Sponsors/>
       <FaqSection />
-      {shouldLoadBot && <WhiteBot />}
+      {shouldLoadBot && <WhiteBot />}  
       <Footer />
     </main>
   );
