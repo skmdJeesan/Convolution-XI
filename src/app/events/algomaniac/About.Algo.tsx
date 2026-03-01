c5e70fb806bcebd420c6f3d9f2954361f33b69ac
SayanIndra83 <sayanindra0@gmail.com>
2026-03-02 02:40:58 +0530

    solo testing successful, revert back

--- src/app/events/algomaniac/About.Algo.tsx
+++ src/app/events/algomaniac/About.Algo.tsx
@@ -17,7 +17,7 @@ export default function About() {
   const [loading, setLoading] = useState(false);
 
   const eventName = "algomaniac";
-  const isClosed = false; // Toggle this to true to shut down registrations
+  const isClosed = true; // Toggle this to true to shut down registrations
 
   const userEvents = contextData?.user?.eventsRegistered || [];
   const isRegistered = userEvents.some(
