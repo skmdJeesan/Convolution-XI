4fff845d6730686306a5dc463762e7687afdb1ca
SayanIndra83 <sayanindra0@gmail.com>
2026-03-03 17:54:00 +0530

    numbers added

--- src/app/events/aboltabol/About.AbolTabol.tsx
+++ src/app/events/aboltabol/About.AbolTabol.tsx
@@ -73,7 +73,7 @@ export default function About() {
   return (
     <section
       id="about"
-      className="relative w-full h-screen flex items-center justify-center pt-20 px-6 pb-10  overflow-hidden "
+      className="relative w-full max-h-screen flex items-center justify-center pt-20 px-6 pb-10  overflow-hidden "
     >
       <div
         className="absolute inset-0 opacity-10 pointer-events-none"
